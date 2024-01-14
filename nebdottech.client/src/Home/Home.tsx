f87c31e248e3abaac2f5f3ae0a267da60393f5db
neb-abera <nebyouabera@gmail.com>
2024-01-13 20:48:23 -0600

    init project, add temp landing page

---
+++ nebdottech.client/src/Home/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Home() {
+  return (
+      <div className="header">
+          <div>
+              <div className="single_slider">
+                  <div className="headshot">
+                  </div>
+                  <div className="todoMessage">
+                      <h1>Hi, I'm <span>Neb</span></h1>
+                      <p>Software Engineer @ Northrop Grumman</p>
+                      <p>This site is currently under development, so visit my <a href="https://www.linkedin.com/in/neb-abera/">LinkedIn</a></p>
+                      <p>or <a href="https://github.com/neb-abera">LinkedIn</a> in the meantime.</p>
+                  </div>
+              </div>
+          </div>
+          {/*<h1 id="tabelLabel">Weather forecast</h1>*/}
+          {/*<p>This component demonstrates fetching data from the server.</p>*/}
+          {/*{contents}*/}
+          <h1>Copyright 2013-2023 colorlib.com | operated by Divilab LLC <a href="https://colorlib.com/privacy-policy/">Privacy Policy</a> | <a href="https://colorlib.com/wp/terms-conditions/">Site Terms</a> & <a href="https://colorlib.com/wp/colorlib-disclaimer/"></a>Disclosures</h1>
+      </div>
+  );
+}
+
+export default Home;
